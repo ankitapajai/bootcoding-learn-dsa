ca143bad315766d7de87fa80eda35f05437f4505
ankitapajai <ankitapajai@gmail.com>
2023-07-13 00:15:25 +0530

    Create linked list...Third class Create list

---
+++ src/main/java/com/bootcoding/dsa/linked_list/CreateList.java
@@ -0,0 +1,16 @@
+package com.bootcoding.dsa.linked_list;
+
+public class CreateList {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.insertElement(10);
+        linkedList.insertElement(20);
+        linkedList.insertElement(30);
+        linkedList.insertElement(40);
+        linkedList.insertElement(50);
+
+        linkedList.display();
+
+    }
+
+}
